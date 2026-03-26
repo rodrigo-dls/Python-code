1d49848eb7583505e7f501e7021decbac6f2d1e1
Rodrigo <rodri.m.dls@gmail.com>
2026-03-26 03:22:10 +0200

    Add files for API module

---
+++ part_2/module_4/exercise_1.py
@@ -0,0 +1,1 @@
+# Write a program that fetches and prints out a random Chuck Norris joke for the user. Use the API presented here: https://api.chucknorris.io/. The user should only be shown the joke text.
