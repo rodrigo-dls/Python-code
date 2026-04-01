83db59af9b17b502db5f8cce10dbab6f5ccb31e4
Rodrigo <rodri.m.dls@gmail.com>
2026-04-01 14:16:02 +0300

    Add exercises of APIs module, module 4 of part 2

--- part_2/module_4/exercise_1.py
+++ part_2/module_4/exercise_1.py
@@ -1,1 +1,13 @@
 # Write a program that fetches and prints out a random Chuck Norris joke for the user. Use the API presented here: https://api.chucknorris.io/. The user should only be shown the joke text.
+
+import requests
+import json
+
+
+request = 'https://api.chucknorris.io/jokes/random'
+response = requests.get(request).json()
+
+# print(response)     # the output is difficult to read
+# print(json.dumps(response, indent=2))    # the function transforms the output to make it easy for the human eye
+
+print(response['value'])
--- part_2/module_4/exercise_2.py
+++ part_2/module_4/exercise_2.py
@@ -1,1 +1,19 @@
 # Familiarize yourself with the OpenWeather weather API at: https://openweathermap.org/api. Your task is to write a program that asks the user for the name of a municipality and then prints out the corresponding weather condition description text and temperature in Celsius degrees. Take a good look at the API documentation. You must register for the service to receive the API key required for making API requests. Furthermore, find out how you can convert Kelvin degrees into Celsius.
+
+import requests
+import json
+
+city_name = input("Enter name of a municipality: ")
+units = 'metric'    # metric units to print temperature in Celsius
+request = f'https://api.openweathermap.org/data/2.5/weather?q={city_name}&units={units}&appid=864acf5017b18fd3f074e343f02400ca'
+
+response = requests.get(request).json()
+
+# print(json.dumps(response, indent=2))   # use the indented format to find the desired 'keys'
+
+print(f"""
+===== Weather report for {city_name} ===== 
+----> {response['weather'][0]['main']} - {response['weather'][0]['description']}
+Temperature:
+----> {response['main']['temp']} celsius
+""")
